2c127f3d60353e66d2a7c2b1d9873951628aba61
David Morrison <me@davemor.com>
2015-01-06 12:55:05 +0000

    Add information about open source code used in app to about screen.

--- app/src/main/java/uk/gov/eastlothian/gowalk/ui/AboutActivity.java
+++ app/src/main/java/uk/gov/eastlothian/gowalk/ui/AboutActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
 
 import uk.gov.eastlothian.gowalk.R;
 
@@ -38,6 +40,10 @@ public class AboutActivity extends MainMenuActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+            TextView about = (TextView) rootView.findViewById(R.id.about_people);
+            about.setMovementMethod(LinkMovementMethod.getInstance());
+
             return rootView;
         }
     }
